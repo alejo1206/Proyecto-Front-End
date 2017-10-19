657b537ca34703f379689d2cedf9f8934961921d
Alejo Cervino <alejocervino@hotmail.com>
2017-10-19 16:46:09 -0300

    agrego un par de componentes basicos y los archivos .json para usar en los listados

--- Proyecto-Front-End/src/app/app.module.ts
+++ Proyecto-Front-End/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
