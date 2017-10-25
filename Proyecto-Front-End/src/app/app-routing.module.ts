ffe05a1269ce6bb62a934da6ee38596660723ba8
Siwi <siwidjc@gmail.com>
2017-10-24 22:02:15 -0300

    router sin andar

--- Proyecto-Front-End/src/app/app-routing.module.ts
+++ Proyecto-Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "*", component: ListComponent},
+  {path: "*/*", component: FormComponent},
+  {path: "*/*/:id", component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
