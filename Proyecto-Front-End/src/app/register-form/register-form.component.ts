05bdb2c62e3d33fadf05dc610605048803440ef6
Alejo Cervino <alejocervino@hotmail.com>
2017-10-29 10:49:48 -0300

    armo metodo submit en register form y creo algunas preguntas genericas para form dinamico

--- Proyecto-Front-End/src/app/register-form/register-form.component.ts
+++ Proyecto-Front-End/src/app/register-form/register-form.component.ts
@@ -17,29 +17,7 @@ export class RegisterFormComponent implements OnInit {
     this.createForm();
   }
 
-  ngOnInit() {
-    this.service.getLast("usuarios").subscribe(data => {
-      let newUser: FormEntity = new FormEntity("", "crear", Number.parseInt(data["id"]) + 1);
-      let labels: string[] = [];
-      let values: string[] = [];
-      for (var key in data) {
-        labels.push(key);
-      }
-      values.push((Number.parseInt(data["id"]) + 1).toString());
-      values.push(this.rForm.controls["username"].value);
-      values.push(this.rForm.controls["password"].value);
-      values.push(this.rForm.controls["firstname"].value);
-      values.push(this.rForm.controls["lastname"].value);
-      values.push(this.rForm.controls["email"].value);
-      values.push("Cliente");
-      values.push("");
-      values.push("");
-      values.push("");
-      newUser.setLabels(labels);
-      newUser.setValues(values);
-      this.service.add(newUser, "usuarios");
-    });
-  }
+  ngOnInit() {}
 
   createForm(){
     this.rForm = this.fb.group({
@@ -74,7 +52,27 @@ export class RegisterFormComponent implements OnInit {
   }
 
   submit(){
-    
+    this.service.getLast("usuarios").subscribe(data => {
+      let newUser: FormEntity = new FormEntity("", "crear", Number.parseInt(data["id"]) + 1);
+      let labels: string[] = [];
+      let values: string[] = [];
+      for (var key in data) {
+        labels.push(key);
+      }
+      values.push((Number.parseInt(data["id"]) + 1).toString());
+      values.push(this.rForm.controls["username"].value);
+      values.push(this.rForm.controls["password"].value);
+      values.push(this.rForm.controls["firstname"].value);
+      values.push(this.rForm.controls["lastname"].value);
+      values.push(this.rForm.controls["email"].value);
+      values.push("Cliente");
+      values.push("");
+      values.push("");
+      values.push("");
+      newUser.setLabels(labels);
+      newUser.setValues(values);
+      this.service.add(newUser, "usuarios");
+    });
   }
 
 }
