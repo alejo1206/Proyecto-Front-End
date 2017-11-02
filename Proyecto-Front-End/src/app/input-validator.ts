eca63cdcd9c2b5cc911d560e2c77d62874815749
Alejo Cervino <alejocervino@hotmail.com>
2017-11-02 12:36:36 -0300

    acomodo validador y agrego mensajes de error

--- Proyecto-Front-End/src/app/input-validator.ts
+++ Proyecto-Front-End/src/app/input-validator.ts
@@ -6,7 +6,7 @@ export class InputValidator{
         let fechaSalida = controls.get("Fecha Salida");
         let horaEntrada = controls.get("Hora Entrada");
         let horaSalida = controls.get("Hora Salida");
-        if(fechaEntrada !== undefined && fechaSalida !== undefined && horaEntrada !== undefined && horaSalida !== undefined && fechaEntrada !== null && fechaSalida !== null && horaEntrada !== null && horaSalida !== null){
+        if(fechaEntrada !== undefined && fechaSalida !== undefined && fechaEntrada.value !== null && fechaSalida.value !== null){
             let entrada: number = Date.parse(fechaEntrada.value);
             let salida: number = Date.parse(fechaSalida.value);
             if(salida < entrada){
@@ -14,16 +14,16 @@ export class InputValidator{
                     fechaEntrada: true
                 }
             }
-            else{
+            else if(horaEntrada !== undefined && horaSalida !== undefined && horaEntrada.value !== null && horaSalida.value !== null){
                 let entradaStr = horaEntrada.value;
                 let salidaStr = horaSalida.value;
                 entradaStr = entradaStr.split(":");
                 salidaStr = salidaStr.split(":");
                 let horaEnt = new Date();
                 let horaSal = new Date();
-                horaEnt.setHours(entradaStr[0],entradaStr[1],entradaStr[2]);
-                horaSal.setHours(salidaStr[0],salidaStr[1],salidaStr[2]);
-                if(horaSal.getTime() <= horaEnt.getTime()){
+                horaEnt.setHours(entradaStr[0],entradaStr[1]);
+                horaSal.setHours(salidaStr[0],salidaStr[1]);
+                if(horaSal.getTime() <= horaEnt.getTime() && entrada === salida){
                     return{
                         horaEntrada: true
                     }
